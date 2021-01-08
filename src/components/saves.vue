869f533b66d454d27e239ebe05507e0dcfabfcae
Fabian <19959@ma-web.nl>
2021-01-08 12:57:49 +0100

    component gemaakt voor de opgeslagen websites

---
+++ src/components/saves.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Opgeslagen websites voor later</h1>
+        <div v-bind:key="save.id" v-for="save in saves">
+            <h3>{{save.naam_site}}</h3>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Saves",
+    props: ["saves"]
+}
+</script>
+<style scoped>
+
+</style>
