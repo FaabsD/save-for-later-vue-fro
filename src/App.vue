2d252a8db50a9fe5b2d0c24f0c05cefcecef3242
Fabian <19959@ma-web.nl>
2021-01-08 12:58:37 +0100

    saves component gekoppeld

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{msg}}
+    <Saves v-bind:saves="saves" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Saves from './components/saves';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Saves
+  },
+  data() {
+    return{
+      saves: [
+        {
+          id: 1,
+          naam_site: "Reddit r/entitledparents",
+          site_url: "https://www.reddit.com/r/entitledparents/",
+          omschrijving: "Een reddit pagina verhalen van mensen die in conflict raakten met (op zijn engels) entitleed parents (sommige zijn waarschijnlijk verzonnen)",
+          bekeken: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
