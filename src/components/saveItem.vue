be0d622c5bf7193434c94160d4f683f1ac14dfb5
Fabian <19959@ma-web.nl>
2021-01-08 13:12:40 +0100

    toon een opgelagen website met de website titel

--- src/components/saveItem.vue
+++ src/components/saveItem.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <p>Opgeslagen website</p>
+    <div class="saved-item">
+        <p>{{save.naam_site}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "SaveItem"
+    name: "SaveItem",
+    props:["save"]
 }
 </script>
 
 <style scoped>
-
+    .saved-item {
+        background-color: lightgrey;
+        padding: 1rem;
+    }
+    .visited {
+        color: red;
+        text-decoration: line-through;
+    }
+    .del {
+        background-color: #ff0000;
+        color: #ffffff;
+        padding: 1.5rem 2rem;
+        cursor: pointer;
+        float: right;
+    }
 </style>
