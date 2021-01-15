41e940fcec85ae165eb12a3fedf8ec9e3766c6f4
Fabian <19959@ma-web.nl>
2021-01-15 13:28:13 +0100

    maak een form en verwerk de ingevoerde data

---
+++ src/components/addSave.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <form @submit="addSave">
+            <input type="text" v-model="naam_site" name="naam_site" placeholder="Naam website">
+            <input type="text" v-model="site_url" name="site_url" placeholder="bijv. http://facebook.com" >
+            <textarea v-model="omschrijving" name="omschrijving" cols="30" rows="10" placeholder="Omschrijving"></textarea>
+            <input type="submit" value="Opslaan" class="knop">
+        </form>
+    </div>
+</template>
+<script>
+import uuid from 'uuid';
+export default {
+    name: "AddSave",
+    data() {
+        return {
+            naam_site: '',
+            site_url: '',
+            omschrijving: ''
+        }
+    },
+    methods: {
+        addSave() {
+            const newSave = {
+                id: uuid.v4(),
+                naam_site: this.naam_site,
+                site_url: this.site_url,
+                omschrijving: this.omschrijving,
+                bekeken: false
+            }
+            // stuur naar parent
+            this.$emit('add-save', newSave);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    input[type="text"] {
+        padding: .5rem;
+    }
+    input[type="submit"] {
+        flex-shrink: 1;
+    }
+</style>
