70ee9059dd98bc1470df940c103bd2f4427da42b
Fabian <19959@ma-web.nl>
2021-01-15 13:29:49 +0100

    Update saves.vue

--- src/components/saves.vue
+++ src/components/saves.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Opgeslagen websites voor later</h1>
+        <!-- <h1>Opgeslagen websites voor later</h1> -->
         <div v-bind:key="save.id" v-for="save in saves">
             <SaveItem v-bind:save="save" v-on:del-save="$emit('del-save', save.id)" />
 
