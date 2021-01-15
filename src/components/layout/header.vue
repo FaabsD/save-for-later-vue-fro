08dae21e83c38e4b499c8b347b39c1e954ef1164
Fabian <19959@ma-web.nl>
2021-01-15 13:28:20 +0100

    maak een header

---
+++ src/components/layout/header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <h1>Opgeslagen Websites</h1>
+    </header>
+</template>
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background-color: aquamarine;
+        color: white;
+        text-align: center;
+        padding: 1rem;
+    }
+    .header h1 {
+        text-transform: uppercase;
+        text-decoration: underline;
+    }
+</style>
